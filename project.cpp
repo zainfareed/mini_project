e4292ca6e7d0a3b9959e203ea5b37164907c05af
zainfareed <zfareed2017@namal.edu.pk>
2018-12-18 14:50:59 +0500

    I have make the functions

--- project.cpp
+++ project.cpp
@@ -5,15 +5,39 @@ int main(int argc, char const *argv[]) {
   int nmbr;
 std::cout << "Enter the number : ";
 std::cin >> nmbr;
-if (nmbr==1) {
-    std::cout << "Add new appointment" << '\n';
+
+if (nmbr==1)
+ {
+    add_new_appointment();
   }
 else if(nmbr==2)
-      std::cout << "List All Appointments" << '\n';
+{
+      list_all_appointments();
+}
 else if(nmbr==3)
-    std::cout << "Quit" << '\n';
-
+{
+    quit();
+}
 
 
   return 0;
+}
+
+void add_new_appointment()
+{
+  std::cout << "Please enter discription : " << '\n';
+  std::cout << "Please enter time : " << '\n';
+  std::cout << "Please enter date : " << '\n';
+  std::cout << "Enter 3 to quit" << '\n';
+}
+
+void list_all_appointments()
+{
+  std::cout << "1. Attend of seminar at 11:30 on 25/11/2018" << '\n';
+  std::cout << "2. Guest Lecture at 12:00 on 11/09/2018" << '\n';
+}
+
+void quit()
+{
+  
 }
